e5859ec90cd25c4a28c69af135f6251150257b0c
Tate Rathje <taterathje@gmail.com>
2015-01-06 02:14:44 -0800

    Added templates for bucket and bucket-list and workd on controllers.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -36,7 +36,7 @@ angular.module('bucketList.controllers', [])
 				});
 			}
 		}
-		])
+	])
 
 	.controller('SignInCtrl', [
 		'$scope', '$rootScope', '$firebaseAuth', '$window',
@@ -80,6 +80,71 @@ angular.module('bucketList.controllers', [])
 				});
 			}
 		}
-		])
+	])
+
+	.controller('myListCtrl', function($rootScope, $scope, $window, $ionicModal, $firebase) {
+		$rootScope.show("Please wait... Processing");
+		$scope.list = [];
+		var bucketListRef = new Firebase($rootScope.baseUrl + escapeEmailAddress($rootScope.userEmail));
+		bucketListRef.on('value', function(snapshot) {
+			var data = snapshot.val();
+
+			$scope.list = []
+
+			for (var key in data) {
+				if (data.hasOwnPropery(key)) {
+					if (data[key].isCompleted == false) {
+						data[key].key = key;
+						$scope.list.push(data[key]);
+					}
+				}
+			}
+
+			if ($scope.list.length == 0) {
+				$scope.noData = true;
+			} else {
+				$scope.noData = false;
+			}
+			$rootScope.hide();
+		});
+
+		$ionicModal.fromTemplateUrl('templates/newItem.html', function(modal) {
+			$scope.newTemplate = modal;
+		});
+
+		$scope.newTask = function() {
+			$scope.newTemplate.show();
+		};
+
+		$scope.markCompleted = function(key) {
+			$rootScope.show("Please wait... Updating List");
+			var itemRef = new Firebase($rootScope.baseUrl + escapeEmailAddress($rootScope.userEmail) + '/' + key);
+			itemRef.update({
+				isCompleted: true
+			}, function(error) {
+				if (error) {
+					$rootScope.hide();
+					$rootScope.notify('Oops! something went wrong. Try again later');
+				} else {
+					$rootScope.hide();
+					$rootScope.notify('Successfully updated');
+				}
+			});
+		};
+
+		$scope.deleteItem = function(key) {
+			$rootScope.show("Please wait... Deleting from List");
+			var itemRef = new Firebase($rootScope.baseUrl + escapeEmailAddress($rootScope.userEmail));
+			bucketListRef.child(key).remove(function(error) {
+				if (error) {
+					$rootScope.hide();
+					$rootScope.notify('Oops! something went wrong. Try again later');
+				} else {
+					$rootScope.hide();
+					$rootScope.notify('Successfully deleted');
+				}
+			});
+		};
+	})
 	
 
